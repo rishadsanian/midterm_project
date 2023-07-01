270d24d66c4aa74dc2b670ee3a6b42830c41e8cf
Rishad Alam <rishad.s.a@gmail.com>
2023-07-01 04:26:20 -0400

    add files and update query requirement

---
+++ public/scripts/login.js
@@ -0,0 +1,37 @@
+
+
+// const $login
+
+// <main style="margin: 1em">
+// <h3 style="font-weight: 600; margin-bottom: 0">Login</h3>
+// <br />
+
+// <form action="/login" method="POST">
+//   <div class="form-group mb-2">
+//     <label for="email" style="font-weight: 500">Email Address</label>
+//     <input
+//       class="form-control"
+//       id="email"
+//       type="email"
+//       name="email"
+//       placeholder="Enter email"
+//       style="width: 300px; margin-bottom: 1em"
+//     />
+
+//     <label for="password" style="font-weight: 500">Password</label>
+//     <input
+//       class="form-control"
+//       id="password"
+//       type="password"
+//       name="password"
+//       placeholder="Password"
+//       style="width: 300px; margin-bottom: 1em"
+//     />
+//   </div>
+//   <div class="form-group">
+//     <button type="submit" class="btn btn-primary" style="width: 5em">
+//       Login
+//     </button>
+//   </div>
+// </form>
+// </main>
