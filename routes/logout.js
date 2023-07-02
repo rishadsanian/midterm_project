9ac0f01efbc32b22464cad935b556b436c8b8d84
Rishad Alam <rishad.s.a@gmail.com>
2023-07-02 12:08:37 -0400

    server bug fix in routes

--- routes/logout.js
+++ routes/logout.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.post('/logout', (req, res) => {
+router.get("/", (req, res) => {
+  //get user db
+  //
+  console.log('need jquery to render login form');
+  res.render("users");
+});
+
+
+router.post('/', (req, res) => {
   req.session = null;
   res.redirect('/');
 });
+module.exports = router;
