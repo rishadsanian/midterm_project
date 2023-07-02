d45ff27e2126a3dc911d9e67fbd94a06c334f18f
Rishad Alam <rishad.s.a@gmail.com>
2023-07-02 08:30:53 -0400

    todo comments and sql function for menu

--- routes/menu-api.js
+++ routes/menu-api.js
@@ -3,9 +3,13 @@ const router  = express.Router();
 const db = require('../db/connection');
 
 router.get('/', (req, res) => {
+  //should be '/:id'
+  // convert this to function
   const query = `SELECT * FROM menu_items WHERE restaurant_id = 1;`;
   console.log(query);
   db.query(query)
+
+
     .then(data => {
       const menu = data.rows;
       console.log(menu);
