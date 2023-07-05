b694d82ca1cc4b7def370a8911ce40d8b552e94d
Rishad Alam <rishad.s.a@gmail.com>
2023-07-04 20:09:31 -0400

    group pairing add restaurant route and sql, update seeds

--- public/scripts/customer-main.js
+++ public/scripts/customer-main.js
@@ -3,18 +3,18 @@
 
 // renderMenu();
 
-// const getRestaurants = () => {
-//   $.get("/api/restaurants", function (data) {
-//     const restaurants = data.restaurants;
-//     console.log(restaurants);
+const getRestaurants = () => {
+  $.get("/api/restaurants", function (data) {
+    const restaurants = data.restaurants;
+    console.log(restaurants);
 
-//     for (const location of restaurants) {
-//       console.log(item.restaurant_id);
-//     }
+    for (const location of restaurants) {
+      console.log(location.restaurant_id);
+    }
 
-//     renderRestaurants(menu, categories);
-//   }).fail(function (xhr, status, error) {
-//     // redirect to /error -> error.ejs
-//     console.log(error);
-//   });
-// };
+    renderRestaurants(menu, categories);
+  }).fail(function (xhr, status, error) {
+    // redirect to /error -> error.ejs
+    console.log(error);
+  });
+};
