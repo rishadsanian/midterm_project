6e07cac309cdeb33bea8dc0e90b96d343eb55823
Rishad Alam <rishad.s.a@gmail.com>
2023-07-06 14:15:50 -0400

    reafactor mentor code and implemented original code

--- routes/cart.js
+++ routes/cart.js
@@ -19,68 +19,63 @@ const db = require("../db/connection");
 const addOrder = async (item) => {
   console.log("Cart in add Order function:", item);
 
-  const {
-    customer_id,
-    restaurant_id,
-    menu_item_id,
-    status_id,
-    quantity,
-    unit_price,
-    ordered_time,
-    cart_id,
-  } = item;
+  // Destructure keys and values into arrays
 
-  const queryString = `INSERT INTO ORDERS (customer_id,restaurant_id,menu_item_id,status_id,quantity,unit_price,ordered_time,cart_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8 )`;
+  const columns = Object.keys(item); //cart
+  const values = Object.values(item); //array with objec
+  const placeholders = [];
 
-  const values = [
-    customer_id,
-    restaurant_id,
-    menu_item_id,
-    status_id,
-    quantity,
-    unit_price,
-    ordered_time,
-    cart_id,
-  ];
+  // Placeholders for parameterized queries
+  for (let i = 0; i < values.length; i++) {
+    placeholders.push(`$${i + 1}`);
+  }
 
-  const insertIntoDB = await db.query(queryString, values);
-
-  if (!insertIntoDB) return { msg: "error" };
-  return { msg: "success" };
-
-  //   return db
-  //     .query(queryString, values)
-  //     .then((result) => result.rows)
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //       throw err;
-  //     });
-
-  //   // Destructure keys and values into arrays
-  //   const columns = Object.keys(item); //cart
-  //   const values = Object.values(item); //array with objec
-  //   const placeholders = [];
-
-  //   // Placeholders for parameterized queries
-  //   for (let i = 0; i < values.length; i++) {
-  //     placeholders.push(`$${i + 1}`);
-  //   }
-
-  //   // Query string
-  //   const qs = `
-  //     INSERT INTO orders (${columns.join(", ")})
-  //     VALUES (${placeholders.join(", ")})
-  //     RETURNING *;
-  //   `;
-  //   console.log("Query String: ", qs);
-  //   return db
-  //     .query(qs, values)
-  //     .then((result) => result.rows)
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //       throw err;
-  //     });
+  // Query string
+  const qs = `
+      INSERT INTO orders (${columns.join(", ")})
+      VALUES (${placeholders.join(", ")})
+      RETURNING *;
+    `;
+  console.log("Query String: ", qs);
+  return db
+    .query(qs, values)
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+      throw err;
+    });
 };
+
+//   const {
+//     customer_id,
+//     restaurant_id,
+//     menu_item_id,
+//     status_id,
+//     quantity,
+//     unit_price,
+//     ordered_time,
+//     cart_id,
+//   } = item;
+
+//   const queryString = `INSERT INTO ORDERS (customer_id,restaurant_id,menu_item_id,status_id,quantity,unit_price,ordered_time,cart_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8 )`;
+
+//   const values = [
+//     customer_id,
+//     restaurant_id,
+//     menu_item_id,
+//     status_id,
+//     quantity,
+//     unit_price,
+//     ordered_time,
+//     cart_id,
+//   ];
+
+//   const insertIntoDB = await db.query(queryString, values);
+
+//   if (!insertIntoDB) return { msg: "error" };
+//   return { msg: "success" };
+
+// };
 
 router.post("/", async (req, res) => {
   const { cart } = req.body;
@@ -97,14 +92,6 @@ router.post("/", async (req, res) => {
 
   console.log("Cart in router post request: ", cart);
   return res.json({ name: 122 });
-  // addOrder(cart)
-  //   .then((response) => {
-  //     res.send(response);
-  //   })
-  //   .catch((e) => {
-  //     console.error(e);
-  //     res.send(e);
-  //   });
 });
 
 module.exports = router;
